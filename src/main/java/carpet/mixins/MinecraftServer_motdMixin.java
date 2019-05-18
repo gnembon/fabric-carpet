3330e6f9229c5d5f6f48516b22ae0babaee521fc
gnembon <gnembonmc@gmail.com>
2019-05-18 00:45:36 -0300

    rework motd and 14.2 pre 2

--- src/main/java/carpet/mixins/MinecraftServer_motdMixin.java
+++ src/main/java/carpet/mixins/MinecraftServer_motdMixin.java
@@ -1,6 +1,7 @@
 package carpet.mixins;
 
 import carpet.CarpetSettings;
+import carpet.fakes.MinecraftServer_motdInterface;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -8,20 +9,33 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(MinecraftServer.class)
-public class MinecraftServer_motdMixin
+public abstract class MinecraftServer_motdMixin implements MinecraftServer_motdInterface
 {
     @Shadow private String motd;
+
+    @Shadow public abstract void setMotd(String string_1);
 
     @Redirect(method = "run", at = @At(
             value = "FIELD",
             target = "Lnet/minecraft/server/MinecraftServer;motd:Ljava/lang/String;")
     )
-    private String setMotd(MinecraftServer server)
+    private String setCustomMotd(MinecraftServer server)
     {
-        if ("_".equals(CarpetSettings.getString("customMOTD")))
-            return motd;
-        else
-            return CarpetSettings.getString("customMOTD");
+        return checkMOTD();
     }
 
+    @Override
+    public String checkMOTD()
+    {
+        if ("_".equals(CarpetSettings.getString("customMOTD")))
+        {
+            setMotd(motd);
+            return motd;
+        }
+        else
+        {
+            setMotd(CarpetSettings.getString("customMOTD"));
+            return CarpetSettings.getString("customMOTD");
+        }
+    }
 }
