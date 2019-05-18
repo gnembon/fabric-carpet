2f6483196bc3d44e41dff8d1d2487c1f37799eb3
DeadlyMC <modding.deadlymc@gmail.com>
2019-05-18 20:20:32 +0530

    added explosionNoBlockDamage

---
+++ src/main/java/carpet/mixins/ExplosionMixin.java
@@ -0,0 +1,32 @@
+package carpet.mixins;
+
+import carpet.CarpetSettings;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.explosion.Explosion;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(Explosion.class)
+public abstract class ExplosionMixin
+{
+    @Shadow
+    @Final
+    private List<BlockPos> affectedBlocks;
+    
+    @Inject(method = "collectBlocksAndDamageEntities", at = @At(value = "INVOKE", shift = At.Shift.AFTER,
+            target = "Ljava/util/List;addAll(Ljava/util/Collection;)Z"))
+    private void clearList(CallbackInfo ci)
+    {
+        if (CarpetSettings.getBool("explosionNoBlockDamage"))
+        {
+            this.affectedBlocks.clear();
+        }
+    }
+    
+}
