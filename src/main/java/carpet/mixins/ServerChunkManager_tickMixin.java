224468964ba37ce2eed6a7ce1cb190810c6fda7d
gnembon <gnembonmc@gmail.com>
2019-06-19 21:37:04 -0300

    1.14.3 pre 4, added back profiling and removed double dispensing for blocks.

--- src/main/java/carpet/mixins/ServerChunkManager_tickMixin.java
+++ src/main/java/carpet/mixins/ServerChunkManager_tickMixin.java
@@ -19,7 +19,7 @@ import java.util.function.BooleanSupplier;
 @Mixin(ServerChunkManager.class)
 public abstract class ServerChunkManager_tickMixin
 {
-    /*
+
     @Shadow @Final private ServerWorld world;
 
     CarpetProfiler.ProfilerToken currentSection;
@@ -66,7 +66,7 @@ public abstract class ServerChunkManager_tickMixin
         {
             CarpetProfiler.end_current_section(currentSection);
         }
-    }*/
+    }
 
     //// Tick freeze
     @Redirect(method = "tickChunks", at = @At(
