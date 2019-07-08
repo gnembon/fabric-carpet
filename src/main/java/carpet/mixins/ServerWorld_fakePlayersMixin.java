3aee2e4b9044996e824742225ce61e2832bc055e
gnembon <gnembonmc@gmail.com>
2019-07-08 00:51:56 -0300

    proper name

--- src/main/java/carpet/mixins/ServerWorld_fakePlayersMixin.java
+++ src/main/java/carpet/mixins/ServerWorld_fakePlayersMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ServerWorld.class)
-public abstract class ServerWorld_fakePlayersDimensionChange
+public abstract class ServerWorld_fakePlayersMixin
 {
     @Shadow private boolean ticking;
 
