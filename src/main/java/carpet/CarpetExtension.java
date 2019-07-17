50d1d74579917ea3d3d5af802185fdd7e6ef9622
gnembon <gnembonmc@gmail.com>
2019-07-17 03:58:16 -0300

    player actions

--- src/main/java/carpet/CarpetExtension.java
+++ src/main/java/carpet/CarpetExtension.java
@@ -2,48 +2,48 @@ package carpet;
 
 import carpet.settings.SettingsManager;
 import com.mojang.brigadier.CommandDispatcher;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
 
-public abstract class CarpetExtension
+public interface CarpetExtension
 {
     /**
      * When game started before world is loaded
      */
-    public void onGameStarted() {}
+    default void onGameStarted() {}
 
     /**
      * Runs once per loaded world once the World files / gamerules etc are fully loaded
      * Can be loaded multiple times in SinglePlayer
      */
-    public void onServerLoaded(MinecraftServer server) {}
+    default void onServerLoaded(MinecraftServer server) {}
 
     /**
      * Runs once per game tick, as a first thing in the tick
      */
-    public void onTick(MinecraftServer server) {}
+    default void onTick(MinecraftServer server) {}
 
     /**
      * Register your own commands right after vanilla commands are added
      * If that matters for you
      */
-    public void registerCommands(CommandDispatcher<ServerCommandSource> dispatcher) {}
+    default void registerCommands(CommandDispatcher<ServerCommandSource> dispatcher) {}
 
     /**
      * Provide your own custom settings manager managed in the same way as base /carpet
      * command, but separated to its own command as defined in SettingsManager.
      */
-    public SettingsManager customSettingsManager() {return null;}
+    default SettingsManager customSettingsManager() {return null;}
 
     /**
      * todiddalidoo
      */
-    public void onPlayerLoggedIn(PlayerEntity player) {}
+    default void onPlayerLoggedIn(ServerPlayerEntity player) {}
 
     /**
      * todiddalidoo
      */
-    public void onPlayerLoggedOut(PlayerEntity player) {}
+    default void onPlayerLoggedOut(ServerPlayerEntity player) {}
 
 }
--- src/main/java/carpet/helpers/Tracer.java
+++ src/main/java/carpet/helpers/Tracer.java
@@ -1,16 +1,90 @@
 package carpet.helpers;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.RayTraceContext;
+import net.minecraft.world.World;
+
+import java.util.Optional;
+import java.util.function.Predicate;
 
 public class Tracer
 {
-    public static HitResult traceEntityLook(Entity entity, double reach) {
-        Vec3d vec3d_1 = entity.getCameraPosVec(1.0F);
-        Vec3d vec3d_2 = entity.getRotationVec(1.0F);
-        Vec3d vec3d_3 = vec3d_1.add(vec3d_2.x * reach, vec3d_2.y * reach, vec3d_2.z * reach);
-        return entity.world.rayTrace(new RayTraceContext(vec3d_1, vec3d_3, RayTraceContext.ShapeType.OUTLINE, RayTraceContext.FluidHandling.ANY, entity));
+    public static HitResult rayTrace(Entity source, float partialTicks, double reach, boolean fluids)
+    {
+        BlockHitResult blockHit = rayTraceBlocks(source, partialTicks, reach, fluids);
+        double maxSqDist = reach * reach;
+        if (blockHit != null)
+        {
+            maxSqDist = blockHit.getPos().squaredDistanceTo(source.getCameraPosVec(partialTicks));
+        }
+        EntityHitResult entityHit = rayTraceEntities(source, partialTicks, reach, maxSqDist);
+        return entityHit == null ? blockHit : entityHit;
+    }
+
+    public static BlockHitResult rayTraceBlocks(Entity source, float partialTicks, double reach, boolean fluids)
+    {
+        Vec3d pos = source.getCameraPosVec(partialTicks);
+        Vec3d rotation = source.getRotationVec(partialTicks);
+        Vec3d reachEnd = pos.add(rotation.x * reach, rotation.y * reach, rotation.z * reach);
+        return source.world.rayTrace(new RayTraceContext(pos, reachEnd, RayTraceContext.ShapeType.OUTLINE, fluids ? RayTraceContext.FluidHandling.ANY : RayTraceContext.FluidHandling.NONE, source));
+    }
+
+    public static EntityHitResult rayTraceEntities(Entity source, float partialTicks, double reach, double maxSqDist)
+    {
+        Vec3d pos = source.getCameraPosVec(partialTicks);
+        Vec3d reachVec = source.getRotationVec(partialTicks).multiply(reach);
+        Box box = source.getBoundingBox().stretch(reachVec).expand(1);
+        return rayTraceEntities(source, pos, pos.add(reachVec), box, e -> !e.isSpectator() && e.collides(), maxSqDist);
+    }
+
+    public static EntityHitResult rayTraceEntities(Entity source, Vec3d start, Vec3d end, Box box, Predicate<Entity> predicate, double maxSqDistance)
+    {
+        World world = source.world;
+        double targetDistance = maxSqDistance;
+        Entity target = null;
+        Vec3d targetHitPos = null;
+        for (Entity current : world.getEntities(source, box, predicate))
+        {
+            Box currentBox = current.getBoundingBox().expand(current.getTargetingMargin());
+            Optional<Vec3d> currentHit = currentBox.rayTrace(start, end);
+            if (currentBox.contains(start))
+            {
+                if (targetDistance >= 0)
+                {
+                    target = current;
+                    targetHitPos = currentHit.orElse(start);
+                    targetDistance = 0;
+                }
+            }
+            else if (currentHit.isPresent())
+            {
+                Vec3d currentHitPos = currentHit.get();
+                double currentDistance = start.squaredDistanceTo(currentHitPos);
+                if (currentDistance < targetDistance || targetDistance == 0)
+                {
+                    if (current.getRootVehicle() == source.getRootVehicle())
+                    {
+                        if (targetDistance == 0)
+                        {
+                            target = current;
+                            targetHitPos = currentHitPos;
+                        }
+                    }
+                    else
+                    {
+                        target = current;
+                        targetHitPos = currentHitPos;
+                        targetDistance = currentDistance;
+                    }
+                }
+            }
+        }
+        if (target == null) return null;
+        return new EntityHitResult(target, targetHitPos);
     }
 }
