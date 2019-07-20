64aca650cf5461dc95dbd622d80dd1d11273fe75
gnembon <gnembonmc@gmail.com>
2019-07-20 19:14:03 -0300

    host tweaks

--- src/main/java/carpet/script/value/NBTSerializableValue.java
+++ src/main/java/carpet/script/value/NBTSerializableValue.java
@@ -257,12 +257,6 @@ public class NBTSerializableValue extends Value
         }
         catch (CommandSyntaxException ignored) { }
         return Value.NULL;
-
-        /*int numitems = items.size();
-        long range = abs(index)/numitems;
-        index += (range+2)*numitems;
-        index = index % numitems;
-        return items.get((int)index); */
     }
 
     @Override
