993c0831982c731638aa87e76a35235c7bc5a107
gnembon <gnembonmc@gmail.com>
2019-07-30 23:01:36 -0300

    antiCheat allows Elytra to deploy when lagging

--- src/main/java/carpet/mixins/ServerPlayNetworkHandler_antiCheatDisabledMixin.java
+++ src/main/java/carpet/mixins/ServerPlayNetworkHandler_antiCheatDisabledMixin.java
@@ -1,8 +1,11 @@
 package carpet.mixins;
 
 import carpet.settings.CarpetSettings;
+import net.minecraft.network.ClientConnection;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,6 +21,10 @@ public abstract class ServerPlayNetworkHandler_antiCheatDisabledMixin
     @Shadow private int vehicleFloatingTicks;
 
     @Shadow protected abstract boolean isServerOwner();
+
+    @Shadow @Final public ClientConnection client;
+
+    @Shadow public ServerPlayerEntity player;
 
     @Inject(method = "tick", at = @At("HEAD"))
     private void restrictFloatingBits(CallbackInfo ci)
@@ -45,6 +52,28 @@ public abstract class ServerPlayNetworkHandler_antiCheatDisabledMixin
     private boolean relaxMoveRestrictions(ServerPlayerEntity serverPlayerEntity)
     {
         return CarpetSettings.antiCheatDisabled || serverPlayerEntity.isInTeleportationState();
+    }
+
+    @Redirect(method = "onClientCommand", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/server/network/ServerPlayerEntity;getVelocity()Lnet/minecraft/util/math/Vec3d;"
+    ))
+    private Vec3d getFallingVelocity(ServerPlayerEntity serverPlayerEntity)
+    {
+        if (CarpetSettings.antiCheatDisabled)
+            return new Vec3d(0,-1,0);
+        return serverPlayerEntity.getVelocity();
+    }
+
+    @Redirect(method = "onClientCommand", at = @At(
+            value = "FIELD",
+            target = "Lnet/minecraft/server/network/ServerPlayerEntity;onGround:Z"
+    ))
+    private boolean getNotOnGround(ServerPlayerEntity serverPlayerEntity)
+    {
+        if (CarpetSettings.antiCheatDisabled)
+            return false;
+        return serverPlayerEntity.onGround;
     }
 
 
