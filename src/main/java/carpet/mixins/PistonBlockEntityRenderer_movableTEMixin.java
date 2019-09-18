ef94e7c302087c67d80fe06d2b1b136afbde45e4
2No2Name <2No2Name@web.de>
2019-09-19 01:07:43 +0200

    Updated MovableBlockEntity Rendering Code

--- src/main/java/carpet/mixins/PistonBlockEntityRenderer_movableTEMixin.java
+++ src/main/java/carpet/mixins/PistonBlockEntityRenderer_movableTEMixin.java
@@ -3,8 +3,10 @@ package carpet.mixins;
 import carpet.settings.CarpetSettings;
 import carpet.fakes.BlockEntityRenderDispatcherInterface;
 import carpet.fakes.PistonBlockEntityInterface;
+import net.minecraft.block.BlockRenderLayer;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.PistonBlockEntity;
+import net.minecraft.client.render.BufferBuilder;
 import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
 import net.minecraft.client.render.block.entity.PistonBlockEntityRenderer;
@@ -12,23 +14,22 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(PistonBlockEntityRenderer.class)
 public abstract class PistonBlockEntityRenderer_movableTEMixin extends BlockEntityRenderer<PistonBlockEntity>
 {
-    /*
     @Inject(method = "method_3576", at = @At(value = "INVOKE",
             target = "Lnet/minecraft/client/render/block/entity/PistonBlockEntityRenderer;method_3575(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/client/render/BufferBuilder;Lnet/minecraft/world/World;Z)Z", ordinal = 3))
-    private void updateRenderBool(PistonBlockEntity pistonBlockEntity_1, double double_1, double double_2,
-            double double_3, float float_1, int int_1, CallbackInfo ci)
+    private void updateRenderBool(PistonBlockEntity pistonBlockEntity_1, double double_1, double double_2, double double_3, float float_1, int int_1, BlockRenderLayer blockRenderLayer_1, BufferBuilder bufferBuilder_1, int int_2, int int_3, CallbackInfo ci)
     {
         if (!((PistonBlockEntityInterface) pistonBlockEntity_1).isRenderModeSet())
             ((PistonBlockEntityInterface) pistonBlockEntity_1).setRenderCarriedBlockEntity(CarpetSettings.movableBlockEntities && ((PistonBlockEntityInterface) pistonBlockEntity_1).getCarriedBlockEntity() != null);
     }
-    
-    @Inject(method = "method_3576", at = @At("RETURN"))
-    private void endMethod3576(PistonBlockEntity pistonBlockEntity_1, double double_1, double double_2, double double_3,
-            float float_1, int int_1, CallbackInfo ci)
+
+
+    @Inject(method = "method_3576", at = @At("RETURN"), locals = LocalCapture.NO_CAPTURE)
+    private void endMethod3576(PistonBlockEntity pistonBlockEntity_1, double double_1, double double_2, double double_3, float float_1, int int_1, BlockRenderLayer blockRenderLayer_1, BufferBuilder bufferBuilder_1, int int_2, int int_3, CallbackInfo ci)
     {
         if (((PistonBlockEntityInterface) pistonBlockEntity_1).getRenderCarriedBlockEntity())
         {
@@ -36,9 +37,8 @@ public abstract class PistonBlockEntityRenderer_movableTEMixin extends BlockEnti
             if (carriedBlockEntity != null)
             {
                 carriedBlockEntity.setPos(pistonBlockEntity_1.getPos());
-                ((BlockEntityRenderDispatcherInterface) BlockEntityRenderDispatcher.INSTANCE).renderBlockEntityOffset(carriedBlockEntity, float_1, int_1, pistonBlockEntity_1.getRenderOffsetX(float_1), pistonBlockEntity_1.getRenderOffsetY(float_1), pistonBlockEntity_1.getRenderOffsetZ(float_1));
+                ((BlockEntityRenderDispatcherInterface) BlockEntityRenderDispatcher.INSTANCE).renderBlockEntityOffset(carriedBlockEntity, float_1, int_1, BlockRenderLayer.field_20799, bufferBuilder_1, pistonBlockEntity_1.getRenderOffsetX(float_1), pistonBlockEntity_1.getRenderOffsetY(float_1), pistonBlockEntity_1.getRenderOffsetZ(float_1));
             }
         }
     }
-    */
 }
