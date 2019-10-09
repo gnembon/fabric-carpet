55f6503807c4536f0ebed62595d4907b3db0fd24
gnembon <gnembonmc@gmail.com>
2019-10-09 08:54:08 -0300

    just sayin

--- src/main/java/carpet/settings/CarpetSettings.java
+++ src/main/java/carpet/settings/CarpetSettings.java
@@ -152,7 +152,8 @@ public class CarpetSettings
 
     @Rule(
             desc = "Honey blocks can stick to each other and other blocks",
-            extra = "but not to slime blocks and vice versa",
+            extra = {"... but not to slime blocks and vice versa.",
+                "In 1.14 that behaviour is applied to gold blocks instead"},
             category = {EXPERIMENTAL, FEATURE}
     )
     public static boolean stickyHoney = false;
