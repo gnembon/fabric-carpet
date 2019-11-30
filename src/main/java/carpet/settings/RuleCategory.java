c2f22a141a108d5ff7ed97447de3c30b878c3014
gnembon <gnembonmc@gmail.com>
2019-11-30 19:45:07 -0400

    dispenser category

--- src/main/java/carpet/settings/RuleCategory.java
+++ src/main/java/carpet/settings/RuleCategory.java
@@ -10,5 +10,6 @@ public class RuleCategory
     public static final String FEATURE = "feature";
     public static final String COMMAND = "command";
     public static final String TNT = "tnt";
+    public static final String DISPENSER = "dispenser";
 
 }
