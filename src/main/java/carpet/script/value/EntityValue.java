de20c14da959b2b3edf459b48cb306bb949cf386
gnembon <gnembonmc@gmail.com>
2019-12-12 23:32:58 -0400

    fixed 1.15 positioning

--- src/main/java/carpet/script/value/EntityValue.java
+++ src/main/java/carpet/script/value/EntityValue.java
@@ -444,31 +444,7 @@ public class EntityValue extends Value
             return;
         if (e instanceof ServerPlayerEntity)
         {
-
-            // TODO next - figure out proper way of informting client about pos changes
-            //((ServerPlayerEntity) e).networkHandler.requestTeleport(e.x, e.y, e.z, e.yaw, e.pitch);
-            Set<PlayerPositionLookS2CPacket.Flag> set_1 = EnumSet.noneOf(PlayerPositionLookS2CPacket.Flag.class);
-            //set_1.add(PlayerPositionLookS2CPacket.Flag.X);
-            //set_1.add(PlayerPositionLookS2CPacket.Flag.Y);
-            //set_1.add(PlayerPositionLookS2CPacket.Flag.Z);
-            /*double prevX = e.getX();
-            double prevY = e.getY();
-            double prevZ = e.getZ();
-            float prevYaw = e.yaw;
-            float prevPitch = e.pitch;*/
-
-            //((ServerPlayerEntity)e).networkHandler.teleportRequest(x - prevX, y - prevY, z - prevZ, yaw - prevYaw, pitch - prevPitch, set_1);
-            ((ServerPlayerEntity)e).networkHandler.teleportRequest(x, y, z, yaw, pitch, set_1);
-
-            /*e.setPositionAnglesAndUpdate(x, y, z, yaw, pitch);
-            ((ServerPlayerEntity) e).networkHandler.sendPacket(new PlayerPositionLookS2CPacket(
-                    x - prevX,
-                    y - prevY,
-                    z - prevZ,
-                    yaw - prevYaw,
-                    pitch - prevPitch,
-                    set_1, -1)
-            );*/
+            ((ServerPlayerEntity)e).networkHandler.teleportRequest(x, y, z, yaw, pitch, EnumSet.allOf(PlayerPositionLookS2CPacket.Flag.class));
         }
         else
         {
