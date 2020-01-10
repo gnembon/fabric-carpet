80b72366eeb30128b7b6a3b204e88ceeefcb3936
gnembon <gnembonmc@gmail.com>
2020-01-10 01:21:14 -0400

    attempt to fix #129

--- src/main/java/carpet/mixins/PistonHandler_pushLimitMixin.java
+++ src/main/java/carpet/mixins/PistonHandler_pushLimitMixin.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-@Mixin(PistonHandler.class)
+@Mixin(value = PistonHandler.class, priority = 69420)  // piston push limit is important for carpet
 public class PistonHandler_pushLimitMixin
 {
     @ModifyConstant(method = "tryMove", constant = @Constant(intValue = 12), expect = 3)
