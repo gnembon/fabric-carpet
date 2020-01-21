4da4f7124ff9b3f69ad676856a6c154367085021
gnembon <gnembonmc@gmail.com>
2020-01-20 22:10:22 -0400

    mapping fixes

--- src/main/java/carpet/mixins/PointOfInterest_scarpetMixin.java
+++ src/main/java/carpet/mixins/PointOfInterest_scarpetMixin.java
@@ -3,10 +3,14 @@ package carpet.mixins;
 import net.minecraft.village.PointOfInterest;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(PointOfInterest.class)
 public interface PointOfInterest_scarpetMixin
 {
     @Accessor("freeTickets")
     int getFreeTickets();
+
+    @Invoker
+    boolean callReserveTicket();
 }
