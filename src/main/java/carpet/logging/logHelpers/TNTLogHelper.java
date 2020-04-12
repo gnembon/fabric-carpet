dd4f2aa12d4f4953a4cee7bbbf9e688161f7a445
X-Green <35984104+X-Green@users.noreply.github.com>
2020-04-12 10:55:22 +0800

    More infomation in TNT logging #1

--- src/main/java/carpet/logging/logHelpers/TNTLogHelper.java
+++ src/main/java/carpet/logging/logHelpers/TNTLogHelper.java
@@ -9,6 +9,8 @@ public class TNTLogHelper
 {
     public boolean initialized;
     private double primedX, primedY, primedZ;
+    private static long lastGametime = 0;
+    private static int tntCount = 0;
     private Vec3d primedAngle;
     /**
      * Runs when the TNT is primed. Expects the position and motion angle of the TNT.
@@ -24,18 +26,30 @@ public class TNTLogHelper
     /**
      * Runs when the TNT explodes. Expects the position of the TNT.
      */
-    public void onExploded(double x, double y, double z)
+    public void onExploded(double x, double y, double z, long gametime)
     {
+        if (!(lastGametime == gametime)){
+            tntCount = 0;
+            lastGametime = gametime;
+        }
+        tntCount++;
         LoggerRegistry.getLogger("tnt").log( (option) -> {
             switch (option)
             {
                 case "brief":
                     return new BaseText[]{Messenger.c(
+                            "g  #" + String.valueOf(tntCount),
+                            "m  @" + String.valueOf(gametime),
+                            "g  : ",
                             "l P ",Messenger.dblt("l",primedX,primedY,primedZ),
                             "w  ",Messenger.dblt("l", primedAngle.x, primedAngle.y, primedAngle.z),
                             "r  E ",Messenger.dblt("r",x, y, z))};
                 case "full":
                     return new BaseText[]{Messenger.c(
+                            "g  #" + String.valueOf(tntCount),
+                            "m  @" + String.valueOf(gametime),
+                            "g  : ",
+                            "g  #" + String.valueOf(tntCount) + ": ",
                             "l P ",Messenger.dblf("l",primedX,primedY,primedZ),
                             "w  ",Messenger.dblf("l", primedAngle.x, primedAngle.y, primedAngle.z),
                             "r  E ",Messenger.dblf("r",x, y, z))};
