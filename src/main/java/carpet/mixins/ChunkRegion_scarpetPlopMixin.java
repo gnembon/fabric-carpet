6618afa5ede458f71850ef3b7ba1af9422255926
gnembon <gnembonmc@gmail.com>
2020-05-25 23:50:28 -0300

    clean log when placing structures and features

--- src/main/java/carpet/mixins/ChunkRegion_scarpetPlopMixin.java
+++ src/main/java/carpet/mixins/ChunkRegion_scarpetPlopMixin.java
@@ -2,19 +2,17 @@ package carpet.mixins;
 
 import carpet.CarpetSettings;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.ChunkRegion;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Chunk.class)
-public abstract class ChunkMixin
+@Mixin(ChunkRegion.class)
+public class ChunkRegion_scarpetPlopMixin
 {
-    // failed to mixin into interface. need to mixin in two places that uses it
-    @Inject(method = "markBlockForPostProcessing(Lnet/minecraft/util/math/BlockPos;)V",
-        at =@At("HEAD"), cancellable = true)
-    private void squashWarnings(BlockPos blockPos_1, CallbackInfo ci)
+    @Inject(method = "markBlockForPostProcessing", at = @At("HEAD"))
+    private void markOrNot(BlockPos blockPos, CallbackInfo ci)
     {
         if (CarpetSettings.skipGenerationChecks) ci.cancel();
     }
