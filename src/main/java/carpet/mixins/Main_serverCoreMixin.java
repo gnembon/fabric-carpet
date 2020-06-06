a8a22e1403f10e3e58f3e1d543bbedba70e671f7
gnembon <gnembonmc@gmail.com>
2020-06-05 21:26:47 -0300

    delaying of carpet startup due to extensions not loaded

--- src/main/java/carpet/mixins/Main_serverCoreMixin.java
+++ src/main/java/carpet/mixins/Main_serverCoreMixin.java
@@ -10,7 +10,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Main.class)
 public class Main_serverCoreMixin
 {
-    @Inject(method = "main", at = @At("HEAD"))
+    @Inject(method = "main", at = @At( // little bit later than main to add chance for extensions to register.
+            value = "INVOKE",
+            target = "Ljoptsimple/OptionParser;parse([Ljava/lang/String;)Ljoptsimple/OptionSet;")
+    )
     private static void onServerStarted(String[] args, CallbackInfo ci)
     {
         CarpetServer.onGameStarted();
