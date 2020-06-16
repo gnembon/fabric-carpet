3273470871abcc54d32fc7ff24b45c0138fa226f
gnembon <gnembonmc@gmail.com>
2020-06-16 17:20:39 -0300

    1.3.31

--- src/main/java/carpet/CarpetSettings.java
+++ src/main/java/carpet/CarpetSettings.java
@@ -34,7 +34,7 @@ import static carpet.settings.RuleCategory.CLIENT;
 @SuppressWarnings("CanBeFinal")
 public class CarpetSettings
 {
-    public static final String carpetVersion = "1.3.30+v200615";
+    public static final String carpetVersion = "1.3.31+v200616";
     public static final Logger LOG = LogManager.getLogger();
     public static boolean skipGenerationChecks = false;
     public static boolean impendingFillSkipUpdates = false;
