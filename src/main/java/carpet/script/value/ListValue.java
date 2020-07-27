df16a3561a785dabb8ef68b717d4aefbc45625d4
gnembon <gnembonmc@gmail.com>
2020-07-27 03:13:31 -0300

    better error message

--- src/main/java/carpet/script/value/ListValue.java
+++ src/main/java/carpet/script/value/ListValue.java
@@ -367,7 +367,7 @@ public class ListValue extends AbstractListValue implements ContainerValueInterf
             return put(where, value, false, true);
         if (condition.equalsIgnoreCase("replace"))
             return put(where, value, true, false);
-        throw new  InternalExpressionException("List 'put' modifier could be either 'insert', or 'replace'");
+        throw new  InternalExpressionException("List 'put' modifier could be either 'insert', 'replace', or extend");
     }
 
     @Override
