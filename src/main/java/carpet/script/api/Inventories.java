677ff2da907443b6528a14404db73f14fed6d4d8
gnembon <gnembonmc@gmail.com>
2020-09-24 03:34:31 -0300

    boi

--- src/main/java/carpet/script/api/Inventories.java
+++ src/main/java/carpet/script/api/Inventories.java
@@ -19,6 +19,7 @@ import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundTag;
