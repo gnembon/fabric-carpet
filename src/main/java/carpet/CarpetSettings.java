5c767230915149884f00b2fa733c16029bef3674
gnembon <gnembonmc@gmail.com>
2020-11-02 15:43:21 -0400

    1.16.4 with 1.4.15

--- src/main/java/carpet/CarpetSettings.java
+++ src/main/java/carpet/CarpetSettings.java
@@ -36,7 +36,7 @@ import static carpet.settings.RuleCategory.CLIENT;
 @SuppressWarnings("CanBeFinal")
 public class CarpetSettings
 {
-    public static final String carpetVersion = "1.4.14+v201022";
+    public static final String carpetVersion = "1.4.15+v201102";
     public static final Logger LOG = LogManager.getLogger();
     public static boolean skipGenerationChecks = false;
     public static boolean impendingFillSkipUpdates = false;
