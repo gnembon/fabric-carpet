77113a4d657ad160f13e6f1bf39638b554832559
altrisi <altrisi.trillosierra@gmail.com>
2020-11-14 20:12:57 +0100

    Remove unnecessary classloader param in BundledModule

--- src/main/java/carpet/script/bundled/BundledModule.java
+++ src/main/java/carpet/script/bundled/BundledModule.java
@@ -25,42 +25,38 @@ public class BundledModule extends Module
      */
     public static BundledModule carpetNative(String scriptName, boolean isLibrary)
     {
-        return fromPath(BundledModule.class.getClassLoader(), "assets/carpet/scripts/", scriptName, isLibrary);
+        return fromPath("assets/carpet/scripts/", scriptName, isLibrary);
     }
     
     /**
      * Creates a new {@link BundledModule} with an app located at a specified place.
      * @see #fromPathWithCustomName(ClassLoader, String, String, boolean)
      * 
-     * @param classLoader The {@link ClassLoader} to use when searching for the path. Can be gotten at 
-     *                    {@link Class#getClassLoader()} from the calling class.
      * @param path A {@link String} being the path to the directory where the app is located.
      * @param scriptName A {@link String} being the name of the script. The extension will be autocompleted
      * @param isLibrary A {@link boolean} Indicating whether or not the script is a library
      * @return The created {@link BundledModule}
      */
-    public static BundledModule fromPath(ClassLoader classLoader, String path, String scriptName, boolean isLibrary) {
-    	return fromPathWithCustomName(classLoader, path+scriptName+(isLibrary?".scl":".sc"), scriptName, isLibrary);
+    public static BundledModule fromPath(String path, String scriptName, boolean isLibrary) {
+    	return fromPathWithCustomName(path+scriptName+(isLibrary?".scl":".sc"), scriptName, isLibrary);
     }
     
     /**
      * Creates a new {@link BundledModule} with an app located at the specified fullPath with a custom name.
      * @see #fromPath(ClassLoader, String, String, boolean)
      * 
-     * @param classLoader The {@link ClassLoader} to use when searching for the path. Can be gotten at 
-     *                    {@link Class#getClassLoader()} from the calling class.
      * @param fullPath A {@link String} being the full path to the app's code, including file and extension.
      * @param scriptName A {@link String} being the custom name for the script.
      * @param isLibrary A {@link boolean} Indicating whether or not the script is a library
      * @return The created {@link BundledModule}
      */
-    public static BundledModule fromPathWithCustomName(ClassLoader classLoader, String fullPath, String customName, boolean isLibrary) {
+    public static BundledModule fromPathWithCustomName(String fullPath, String customName, boolean isLibrary) {
     	BundledModule module = new BundledModule(null, null, isLibrary);
     	try
     	{
             module.name = customName.toLowerCase(Locale.ROOT);
             module.code = IOUtils.toString(
-                    classLoader.getResourceAsStream(fullPath),
+            		BundledModule.class.getClassLoader().getResourceAsStream(fullPath),
                     StandardCharsets.UTF_8
             );
         }
