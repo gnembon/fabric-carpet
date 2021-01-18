f1a2a8dbf86cc91f39d4b4174256ced0ef1c62d8
Ramid Khan <ramidzkh@gmail.com>
2021-01-18 14:15:54 +1100

    Improve code

--- src/main/java/carpet/utils/FabricAPIHooks.java
+++ src/main/java/carpet/utils/FabricAPIHooks.java
@@ -3,35 +3,36 @@ package carpet.utils;
 import carpet.network.CarpetClient;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.fabricmc.loader.api.FabricLoader;
 
+import java.util.function.Supplier;
+
 public class FabricAPIHooks {
+
+    private static final boolean CLIENT = FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT;
+    public static final boolean WORLD_RENDER_EVENTS = CLIENT && exists(() -> WorldRenderEvents.class);
 
     private FabricAPIHooks() {
     }
 
     public static void initialize() {
-        if (FabricLoader.getInstance().isModLoaded("fabric")) {
-            if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
-                initializeClient();
-            }
+        if (WORLD_RENDER_EVENTS) {
+            WorldRenderEvents.BEFORE_DEBUG_RENDER.register(context -> {
+                if (CarpetClient.shapes != null) {
+                    RenderSystem.pushMatrix();
+                    CarpetClient.shapes.render(context.camera(), context.tickDelta());
+                    RenderSystem.popMatrix();
+                }
+            });
         }
     }
 
-    @Environment(EnvType.CLIENT)
-    private static void initializeClient() {
-        WorldRenderEvents.BEFORE_DEBUG_RENDER.register(FabricAPIHooks::beforeDebugRender);
-    }
-
-    @Environment(EnvType.CLIENT)
-    private static void beforeDebugRender(WorldRenderContext context) {
-        if (CarpetClient.shapes != null) {
-            RenderSystem.pushMatrix();
-            CarpetClient.shapes.render(context.camera(), context.tickDelta());
-            RenderSystem.popMatrix();
+    private static boolean exists(Supplier<Class<?>> supplier) {
+        try {
+            return supplier.get() != null;
+        } catch (NoClassDefFoundError error) {
+            return false;
         }
     }
 }
