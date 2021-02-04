5d90429fa8a173004d756bf4417b3e70f96f45e0
altrisi <altrisi.trillosierra@gmail.com>
2021-02-04 17:07:42 +0100

    Fix rules-to-wiki

--- src/main/java/carpet/utils/RulePrinter.java
+++ src/main/java/carpet/utils/RulePrinter.java
@@ -1,20 +1,18 @@
 package carpet.utils;
 
 import carpet.CarpetServer;
-import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
+import net.fabricmc.api.DedicatedServerModInitializer;
 import java.lang.System;
 /**
  * Runs only from GitHub Actions to generate the wiki
  * page with all Carpet rules on it.
  * It is here so it can be managed by the IDE
- * 
- * @author altrisi
  *
  */
-public class RulePrinter implements PreLaunchEntrypoint {
+public class RulePrinter implements DedicatedServerModInitializer {
 
     @Override
-    public void onPreLaunch() {
+    public void onInitializeServer() {
         CarpetServer.onGameStarted();
         CarpetServer.settingsManager.printAllRulesToLog(null);
         System.exit(0);
