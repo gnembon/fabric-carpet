75bc21ca7d8f494d62130fd56de48fd379c0471c
Ghoulboy78 <ghoulboy78@gmail.com>
2021-02-14 16:55:25 +0100

    Fixes #687
    Fixes #687

--- src/main/java/carpet/helpers/HopperCounter.java
+++ src/main/java/carpet/helpers/HopperCounter.java
@@ -4,6 +4,7 @@ import carpet.utils.Messenger;
 import com.google.common.collect.Maps;
 import it.unimi.dsi.fastutil.objects.Object2LongLinkedOpenHashMap;
 import it.unimi.dsi.fastutil.objects.Object2LongMap;
+import net.minecraft.block.MaterialColor;
 import net.minecraft.util.DyeColor;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -35,6 +36,7 @@ public class HopperCounter
     }
 
     public final DyeColor color;
+    private final String prettyColour;
     private final Object2LongMap<Item> counter = new Object2LongLinkedOpenHashMap<>();
     private long startTick;
     private long startMillis;
@@ -43,6 +45,7 @@ public class HopperCounter
     private HopperCounter(DyeColor color)
     {
         this.color = color;
+        this.prettyColour = getPrettyColour(color);
         // pubSubProvider = new PubSubInfoProvider<>(QuickCarpet.PUBSUB, "carpet.counter." + color.getName(), 0, this::getTotalItems);
     }
 
@@ -100,9 +103,9 @@ public class HopperCounter
         {
             if (brief)
             {
-                return Collections.singletonList(Messenger.c("g "+color+": -, -/h, - min "));
+                return Collections.singletonList(Messenger.c("g ", prettyColour,"w : -, -/h, - min "));
             }
-            return Collections.singletonList(Messenger.s(String.format("No items for %s yet", color.getName())));
+            return Collections.singletonList(Messenger.c("w No items for ", prettyColour, "w  yet"));
         }
         long total = getTotalItems();
         long ticks = Math.max(realTime ? (System.currentTimeMillis() - startMillis) / 50 : server.getWorld(World.OVERWORLD).getTime() - startTick, 1);  //OW
@@ -110,20 +113,20 @@ public class HopperCounter
         {
             if (brief)
             {
-                return Collections.singletonList(Messenger.c(String.format("c %s: 0, 0/h, %.1f min ", color, ticks / (20.0 * 60.0))));
+                return Collections.singletonList(Messenger.c(prettyColour,String.format("c : 0, 0/h, %.1f min ", ticks / (20.0 * 60.0))));
             }
-            return Collections.singletonList(Messenger.c(String.format("w No items for %s yet (%.2f min.%s)",
-                    color.getName(), ticks / (20.0 * 60.0), (realTime ? " - real time" : "")),
+            return Collections.singletonList(Messenger.c("w No items for ", prettyColour, String.format("w  yet (%.2f min.%s)",
+                    ticks / (20.0 * 60.0), (realTime ? " - real time" : "")),
                     "nb  [X]", "^g reset", "!/counter " + color.getName() +" reset"));
         }
         if (brief)
         {
-            return Collections.singletonList(Messenger.c(String.format("c %s: %d, %d/h, %.1f min ",
-                    color.getName(), total, total * (20 * 60 * 60) / ticks, ticks / (20.0 * 60.0))));
+            return Collections.singletonList(Messenger.c(prettyColour,String.format("c : %d, %d/h, %.1f min ",
+                    total, total * (20 * 60 * 60) / ticks, ticks / (20.0 * 60.0))));
         }
         List<BaseText> items = new ArrayList<>();
-        items.add(Messenger.c(String.format("w Items for %s (%.2f min.%s), total: %d, (%.1f/h):",
-                color, ticks*1.0/(20*60), (realTime?" - real time":""), total, total*1.0*(20*60*60)/ticks),
+        items.add(Messenger.c("w Items for ", prettyColour, String.format("w  (%.2f min.%s), total: %d, (%.1f/h):",
+                ticks*1.0/(20*60), (realTime?" - real time":""), total, total*1.0*(20*60*60)/ticks),
                 "nb [X]", "^g reset", "!/counter "+color+" reset"
         ));
         items.addAll(counter.object2LongEntrySet().stream().sorted((e, f) -> Long.compare(f.getLongValue(), e.getLongValue())).map(e ->
@@ -153,5 +156,45 @@ public class HopperCounter
     public long getTotalItems()
     {
         return counter.values().stream().mapToLong(Long::longValue).sum();
+    }
+
+    private static String getPrettyColour(DyeColor colour){
+        return getPrettyColour(colour, "");
+    }
+
+    private static String getPrettyColour(DyeColor colour, String extra){
+        if (MaterialColor.WHITE.equals(colour.getMaterialColor())) {
+            return "w " + colour.getName() + extra;
+        } else if (MaterialColor.ORANGE.equals(colour.getMaterialColor())) {
+            return "#F9801D " + colour.getName();//cos not specific code for orange
+        } else if (MaterialColor.MAGENTA.equals(colour.getMaterialColor())) {
+            return "m " + colour.getName() + extra;
+        } else if (MaterialColor.LIGHT_BLUE.equals(colour.getMaterialColor())) {
+            return "t " + colour.getName() + extra;
+        } else if (MaterialColor.YELLOW.equals(colour.getMaterialColor())) {
+            return "y " + colour.getName() + extra;
+        } else if (MaterialColor.LIME.equals(colour.getMaterialColor())) {
+            return "l " + colour.getName() + extra;
+        } else if (MaterialColor.PINK.equals(colour.getMaterialColor())) {
+            return "#F38BAA " + colour.getName() + extra;
+        } else if (MaterialColor.GRAY.equals(colour.getMaterialColor())) {
+            return "f " + colour.getName() + extra;
+        } else if (MaterialColor.LIGHT_GRAY.equals(colour.getMaterialColor())) {
+            return "g " + colour.getName() + extra;
+        } else if (MaterialColor.CYAN.equals(colour.getMaterialColor())) {
+            return "c " + colour.getName() + extra;
+        } else if (MaterialColor.PURPLE.equals(colour.getMaterialColor())) {
+            return "p " + colour.getName() + extra;
+        } else if (MaterialColor.BLUE.equals(colour.getMaterialColor())) {
+            return "v " + colour.getName() + extra;
+        } else if (MaterialColor.BROWN.equals(colour.getMaterialColor())) {
+            return "#835432 " + colour.getName() + extra;
+        } else if (MaterialColor.GREEN.equals(colour.getMaterialColor())) {
+            return "e " + colour.getName() + extra;
+        } else if (MaterialColor.RED.equals(colour.getMaterialColor())) {
+            return "r " + colour.getName() + extra;
+        } else {//if black
+            return "k " + colour.getName() + extra;
+        }
     }
 }
