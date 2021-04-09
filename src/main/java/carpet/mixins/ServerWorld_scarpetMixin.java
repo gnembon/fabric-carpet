93c989e2b6389edde4eeb07e3808d951874a8dad
gnembon <gnembonmc@gmail.com>
2021-04-09 01:42:57 -0300

    added `create_explosion` and more event handlers.

--- src/main/java/carpet/mixins/ServerWorld_scarpetMixin.java
+++ src/main/java/carpet/mixins/ServerWorld_scarpetMixin.java
@@ -18,7 +18,6 @@ import net.minecraft.world.explosion.ExplosionBehavior;
 import net.minecraft.world.level.ServerWorldProperties;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
