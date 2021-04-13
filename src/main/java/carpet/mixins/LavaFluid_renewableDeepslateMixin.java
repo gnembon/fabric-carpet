c878ffd6374be50fa77afd4f86e1d6c6a46de41d
replaceitem <40722305+replaceitem@users.noreply.github.com>
2021-04-13 15:44:49 +0200

    Added renewableDeepslate (#806)
    * Added renewableDeepslate
    
    * Check for stillness
    
    Co-authored-by: gnembon <41132274+gnembon@users.noreply.github.com>

---
+++ src/main/java/carpet/mixins/LavaFluid_renewableDeepslateMixin.java
@@ -0,0 +1,30 @@
+package carpet.mixins;
+
+import carpet.CarpetSettings;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.LavaFluid;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LavaFluid.class)
+public abstract class LavaFluid_renewableDeepslateMixin {
+    @Shadow protected abstract void playExtinguishEvent(WorldAccess world, BlockPos pos);
+
+    @Inject(method = "flow", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;getDefaultState()Lnet/minecraft/block/BlockState;"), cancellable = true)
+    private void generateDeepslate(WorldAccess world, BlockPos pos, BlockState state, Direction direction, FluidState fluidState, CallbackInfo ci) {
+        if(CarpetSettings.renewableDeepslate && pos.getY() < 0 && ((World)world).getRegistryKey() == World.OVERWORLD) {
+            world.setBlockState(pos, Blocks.DEEPSLATE.getDefaultState(), 3);
+            this.playExtinguishEvent(world, pos);
+            ci.cancel();
+        }
+    }
+}
