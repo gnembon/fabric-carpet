d3295e6b4ed863822da63addfa0b9acba5689440
gnembon <gnembonmc@gmail.com>
2021-04-14 15:07:08 -0300

    21w15a

--- src/main/java/carpet/mixins/BaseRangeDecorator2_cleanLogsMixin.java
+++ src/main/java/carpet/mixins/BaseRangeDecorator2_cleanLogsMixin.java
@@ -1,18 +1,18 @@
 package carpet.mixins;
 
 import carpet.CarpetSettings;
-import net.minecraft.class_6343;
+import net.minecraft.class_6342;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(class_6343.class)
-public class BaseRangeDecorator_cleanLogsMixin {
+@Mixin(class_6342.class)
+public class BaseRangeDecorator2_cleanLogsMixin {
 
     //@SuppressWarnings("UnresolvedMixinReference")
     @Redirect(method = "get",
-     at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V")
+            at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V")
     )
     private void skipLogs(Logger logger, String message, Object p0)
     {
