f3be908732c4044fe6266696f90e237f495c103e
Ghoulboy78 <ghoulboy27@gmail.com>
2021-04-22 18:29:33 +0200

    Fixing typos

--- src/main/java/carpet/helpers/ScriptDownloader.java
+++ src/main/java/carpet/helpers/ScriptDownloader.java
@@ -9,18 +9,25 @@ import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.LiteralText;
 import net.minecraft.util.WorldSavePath;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.file.Path;
-import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
 
 public class ScriptDownloader {
 
     public static int downloadScript(CommandContext<ServerCommandSource> cc, String path){
-        String code = getScriptCode(cc, path);
+        String code = getScriptCode(path);
         return saveScriptToFile(path, code, cc.getSource().getMinecraftServer(),true);
     }
 
@@ -64,5 +71,26 @@ public class ScriptDownloader {
         }
         Messenger.m(server.getCommandSource(), "gi Successfuly created "+ name+ ".sc in " + location);
         return 1;
+    }
+
+    // converting stream to string
+    private static String getStringFromStream(InputStream inputStream) throws IOException {
+        if (inputStream != null) {
+            Writer stringWriter = new StringWriter();
+
+            char[] charBuffer = new char[2048];
+            try{
+                Reader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+                int counter;
+                while ((counter = reader.read(charBuffer)) != -1) {
+                    stringWriter.write(charBuffer, 0, counter);
+                }
+            } finally {
+                inputStream.close();
+            }
+            return stringWriter.toString();
+        } else {
+            return "";
+        }
     }
 }
