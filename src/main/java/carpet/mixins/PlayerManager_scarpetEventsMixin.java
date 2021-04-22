e8f135872dae0e1892bd4e889e8961fd6cb517ab
gnembon <gnembonmc@gmail.com>
2021-04-22 08:41:27 -0300

    fixes #827

--- src/main/java/carpet/mixins/PlayerManager_scarpetEventsMixin.java
+++ src/main/java/carpet/mixins/PlayerManager_scarpetEventsMixin.java
@@ -23,8 +23,8 @@ public class PlayerManager_scarpetEventsMixin
 
     @Inject(method = "respawnPlayer", at = @At(
             value = "INVOKE",
-            //target = "Lnet/minecraft/server/network/ServerPlayerEntity;onSpawn()V"
-            target = "Lnet/minecraft/server/network/ServerPlayerEntity;method_34225()V"
+            target = "Lnet/minecraft/server/network/ServerPlayerEntity;onSpawn()V"
+            //target = "Lnet/minecraft/server/network/ServerPlayerEntity;method_34225()V"
     ))
     private void invalidatePreviousInstance(ServerPlayerEntity player, boolean alive, CallbackInfoReturnable<ServerPlayerEntity> cir)
     {
