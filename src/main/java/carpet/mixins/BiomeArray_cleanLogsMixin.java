dfa826bb24f8fe084ee511ee5ee0f2c19b32bcfc
gnembon <gnembonmc@gmail.com>
2021-05-05 20:34:59 -0300

    21w18a

--- src/main/java/carpet/mixins/BiomeArray_cleanLogsMixin.java
+++ src/main/java/carpet/mixins/BiomeArray_cleanLogsMixin.java
@@ -10,13 +10,14 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(BiomeArray.class)
 public class BiomeArray_cleanLogsMixin
 {
+    // this changed in 21w18a - maybe its not needed anymore, please check
     @SuppressWarnings("UnresolvedMixinReference")
     @Redirect(method = "<init>(Lnet/minecraft/util/collection/IndexedIterable;Lnet/minecraft/world/HeightLimitView;[I)V", at = @At(
             value = "INVOKE",
-            target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V")
+            target = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V")
     )
-    private void skipLog(Logger logger, String message, Object p0)
+    private void skipLog(Logger logger, String message, Object p0, Object p1)
     {
-        if (!CarpetSettings.cleanLogs) logger.warn(message, p0);
+        if (!CarpetSettings.cleanLogs) logger.warn(message, p0, p1);
     }
 }
