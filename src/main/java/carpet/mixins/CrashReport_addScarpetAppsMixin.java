4ccae7abe0c8608abc648d1fd168b06576c65464
gnembon <gnembonmc@gmail.com>
2021-06-01 19:53:35 -0300

    1.17-pre3

--- src/main/java/carpet/mixins/CrashReport_addScarpetAppsMixin.java
+++ src/main/java/carpet/mixins/CrashReport_addScarpetAppsMixin.java
@@ -3,7 +3,7 @@ package carpet.mixins;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
-import net.minecraft.class_6396;
+import net.minecraft.util.SystemDetails;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,15 +12,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import carpet.CarpetServer;
 
-@Mixin(class_6396.class)
+@Mixin(SystemDetails.class)
 public abstract class CrashReport_addScarpetAppsMixin
 {
-    @Shadow public abstract void method_37123(String string, Supplier<String> supplier);
+    @Shadow public abstract void addSection(String name, Supplier<String> valueSupplier);
 
     @Inject(method = "<init>", at = @At("RETURN"))
     private void fillSystemDetails(CallbackInfo info) {
         if (CarpetServer.scriptServer == null || CarpetServer.scriptServer.modules.isEmpty()) return;
-        method_37123("Loaded Scarpet Apps", () -> {
+        addSection("Loaded Scarpet Apps", () -> {
             return CarpetServer.scriptServer.modules.keySet().stream().collect(Collectors.joining("\n\t\t", "\n\t\t", ""));
         });
     }
