a9a119e172be6fc5186ea28f9c364ebc95a28cb9
gnembon <gnembonmc@gmail.com>
2021-07-09 22:16:29 -0300

    default foliage color has to be fixed on the server since its actually part of client resourcepack. Fixes #992

--- src/main/java/carpet/script/utils/BiomeInfo.java
+++ src/main/java/carpet/script/utils/BiomeInfo.java
@@ -21,20 +21,13 @@ import java.util.function.BiFunction;
 
 public class BiomeInfo
 {
-    // copy of from Biome.getDefaultFoliageColor
-    private static int getDefaultFoliageColor(Biome b) {
-        double d = MathHelper.clamp(b.getTemperature(), 0.0F, 1.0F);
-        double e = MathHelper.clamp(b.getDownfall(), 0.0F, 1.0F);
-        return FoliageColors.getColor(d, e);
-    }
-
     public final static Map<String, BiFunction<ServerWorld, Biome, Value>> biomeFeatures = new HashMap<String, BiFunction<ServerWorld, Biome, Value>>(){{
         put("top_material", (w, b) -> new BlockValue( b.getGenerationSettings().getSurfaceConfig().getTopMaterial(), null, null));
         put("under_material", (w, b) -> new BlockValue( b.getGenerationSettings().getSurfaceConfig().getUnderMaterial(), null, null));
         put("category", (w, b) -> StringValue.of(b.getCategory().getName()));
         put("temperature", (w, b) -> NumericValue.of(b.getTemperature()));
         put("fog_color", (w, b) -> ValueConversions.ofRGB(((BiomeEffectsInterface)b.getEffects()).getCMFogColor()));
-        put("foliage_color", (w, b) -> ValueConversions.ofRGB(((BiomeEffectsInterface)b.getEffects()).getCMFoliageColor().orElseGet(() -> getDefaultFoliageColor(b))));
+        put("foliage_color", (w, b) -> ValueConversions.ofRGB(((BiomeEffectsInterface)b.getEffects()).getCMFoliageColor().orElse(4764952))); // client Biome.getDefaultFoliageColor
         put("sky_color", (w, b) -> ValueConversions.ofRGB(((BiomeEffectsInterface)b.getEffects()).getCMSkyColor()));
         put("water_color", (w, b) -> ValueConversions.ofRGB(((BiomeEffectsInterface)b.getEffects()).getCMWaterColor()));
         put("water_fog_color", (w, b) -> ValueConversions.ofRGB(((BiomeEffectsInterface)b.getEffects()).getCMWaterFogColor()));
