b93e3238b78da96a38dea4bb618387eb00765f5f
gnembon <gnembonmc@gmail.com>
2021-11-04 00:46:27 +0100

    1.4.52

--- src/main/java/carpet/CarpetSettings.java
+++ src/main/java/carpet/CarpetSettings.java
@@ -43,7 +43,7 @@ import static carpet.settings.RuleCategory.CLIENT;
 @SuppressWarnings("CanBeFinal")
 public class CarpetSettings
 {
-    public static final String carpetVersion = "1.4.51+v211028";
+    public static final String carpetVersion = "1.4.52+v211104";
     public static final Logger LOG = LogManager.getLogger("carpet");
     public static ThreadLocal<Boolean> skipGenerationChecks = ThreadLocal.withInitial(() -> false);
     public static ThreadLocal<Boolean> impendingFillSkipUpdates = ThreadLocal.withInitial(() -> false);
