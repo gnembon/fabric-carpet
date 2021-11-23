0dbc5206f9d761e211ec1354a66d6ec110317c1f
Crec0 <83436716+Crec0@users.noreply.github.com>
2021-11-22 16:41:12 -0800

    fix: wrong implementation for absorption

--- src/main/java/carpet/mixins/ExperienceOrbEntityMixin.java
+++ src/main/java/carpet/mixins/ExperienceOrbEntityMixin.java
@@ -3,24 +3,45 @@ package carpet.mixins;
 import carpet.CarpetSettings;
 import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ExperienceOrbEntity.class)
 public abstract class ExperienceOrbEntityMixin {
     @Shadow
     private int pickingCount;
 
-    @Redirect(method = "onPlayerCollision", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;addExperience(I)V"))
-    void addXP(PlayerEntity player, int experience) {
+    @Shadow
+    private int amount;
+
+    @Shadow
+    protected abstract int repairPlayerGears(PlayerEntity player, int amount);
+
+    @Inject(
+            method = "onPlayerCollision",
+            at = @At(
+                    value = "FIELD",
+                    opcode = Opcodes.PUTFIELD,
+                    shift = At.Shift.AFTER,
+                    target = "net/minecraft/entity/ExperienceOrbEntity.pickingCount:I"
+            )
+    )
+    void addXP(PlayerEntity player, CallbackInfo ci) {
         if (CarpetSettings.xpNoCooldown) {
-            player.experiencePickUpDelay = 0;
-            player.addExperience(experience * this.pickingCount);
-            this.pickingCount = 1;
-        } else {
-            player.addExperience(experience);
+            int remainder;
+            while (this.pickingCount > 0) {
+                remainder = this.repairPlayerGears(player, this.amount);
+                if (remainder > 0) {
+                    player.addExperience(remainder);
+                }
+                player.experiencePickUpDelay = 0;
+                this.pickingCount--;
+            }
         }
     }
 }
