3c8d690219a9b47c1fb0d8d87faec144b310ceb5
Crec0 <83436716+Crec0@users.noreply.github.com>
2021-11-27 11:09:31 -0800

    fix: injection point to be within condition

--- src/main/java/carpet/mixins/ExperienceOrbEntityMixin.java
+++ src/main/java/carpet/mixins/ExperienceOrbEntityMixin.java
@@ -3,12 +3,10 @@ package carpet.mixins;
 import carpet.CarpetSettings;
 import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ExperienceOrbEntity.class)
@@ -22,11 +20,18 @@ public abstract class ExperienceOrbEntityMixin {
     @Shadow
     protected abstract int repairPlayerGears(PlayerEntity player, int amount);
 
-    @Inject(method = "onPlayerCollision", at = @At("HEAD"))
+    @Inject(
+            method = "onPlayerCollision",
+            at = @At(
+                    value = "FIELD",
+                    target = "net/minecraft/entity/player/PlayerEntity.experiencePickUpDelay:I",
+                    ordinal = 0
+            )
+    )
     private void addXP(PlayerEntity player, CallbackInfo ci) {
         if (CarpetSettings.xpNoCooldown) {
             player.experiencePickUpDelay = 0;
-            // reducing to 1 and leaving vanilla to deal with it
+            // reducing the count to 1 and leaving vanilla to deal with it
             while (this.pickingCount > 1) {
                 int remainder = this.repairPlayerGears(player, this.amount);
                 if (remainder > 0) {
