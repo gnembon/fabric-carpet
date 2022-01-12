3f6ddc43e5e04400d36ac48eecfbde3c73f2f2ef
replaceitem <40722305+replaceitem@users.noreply.github.com>
2022-01-12 22:52:38 +0100

    Added screen handler system (#888)
    * Added screen handler value
    
    * Added screen handler constructor and function to show to player
    
    * Made inventory_* functions work with screen handlers
    
    * Event callbacks and some refactoring
    
    * Exception if screen has no inventory
    
    * Fixed NPE
    
    * Close screen when null
    
    * Added screen_property
    
    * UNKNOWN_SCREEN throwable
    
    * Fix error when not specifying click callback
    
    * Access to non-container screen handler slots via the inventory_* functions
    
    * Change lectern page
    
    * Added button event
    
    * Added 'close' event and refactoring callback
    
    * Beacon and Brewing screens
    
    * Cartography table screen
    
    * Crafting screen
    
    * Enchantment screen
    
    * Grindstone
    
    * Loom
    
    * Merchant
    
    * Shulkerbox
    
    * Smithing table
    
    * Stonecutter
    
    * Use strings as screen type instead of ScreenHandlerType
    
    * Remove support for multiple players
    
    * Combining create_screen and open_screen and refactoring
    
    * Allow modifying cursor stack
    
    * Cleanup
    
    * Docs
    
    * Added multiple screen examples
    
    * Rename to screen
    
    * Added smoker and blast furnace
    
    * Serialize screen content
    
    * Store host name instead of context
    
    * Add button value special cases to docs
    
    * Spelling and grammar fixes by @James103
    
    * Optimize screen inventory access
    
    * Use -1 for slot click outside the screen
    
    * As boolean, ScreenValue equals isOpen
    
    * Revert "Use -1 for slot click outside the screen"
    
    This reverts commit 68c113e71bb653191cedf4cc356ab7705838dc25.
    
    * Rework of callback function
    
    * Improve screen functionality with drop_item
    
    * Editor keywords
    
    * Only check syncId
    
    * slot_update event callback
    
    * Screem typo
    
    * Mistake
    
    * toTag returns "null" NBT for  closed screen
    
    * Use null for slot index of -999
    
    Co-authored-by: replaceitem <replaceitem.mc@gmail.com>

---
+++ src/main/java/carpet/mixins/ScreenHandlerSubclasses_scarpetMixin.java
@@ -0,0 +1,29 @@
+package carpet.mixins;
+
+import carpet.fakes.ScreenHandlerInterface;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.screen.EnchantmentScreenHandler;
+import net.minecraft.screen.LecternScreenHandler;
+import net.minecraft.screen.LoomScreenHandler;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.screen.StonecutterScreenHandler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+
+//classes that override onButtonClick
+@Mixin({EnchantmentScreenHandler.class, LecternScreenHandler.class, LoomScreenHandler.class, StonecutterScreenHandler.class})
+public abstract class ScreenHandlerSubclasses_scarpetMixin extends ScreenHandler {
+    protected ScreenHandlerSubclasses_scarpetMixin(ScreenHandlerType<?> type, int syncId) {
+        super(type, syncId);
+    }
+
+    @Inject(method = "onButtonClick", at = @At("HEAD"), cancellable = true)
+    private void buttonClickCallback(PlayerEntity player, int id, CallbackInfoReturnable<Boolean> cir) {
+        if(((ScreenHandlerInterface) this).callButtonClickListener(id,player))
+            cir.cancel();
+    }
+}
