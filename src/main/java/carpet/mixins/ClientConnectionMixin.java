ec47dea267f5a409a06c6b609df879f9d53971e8
altrisi <altrisi.trillosierra@gmail.com>
2022-01-19 21:28:36 +0100

    Make fake players compatible with adventure platform lib and minor refactor around them (#1235)
    * Make fake players compatible with adventure platform and minor refactor around them
    
    Cleaner approach
    
    * Remove no longer needed enderpearl mixin for fake players
    
    Their ClientConnection now has an open channel so isOpen returns true

--- src/main/java/carpet/mixins/ClientConnectionMixin.java
+++ src/main/java/carpet/mixins/ClientConnectionMixin.java
@@ -1,18 +1,21 @@
 package carpet.mixins;
 
+import carpet.fakes.ClientConnectionInterface;
 import carpet.logging.logHelpers.PacketCounter;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.util.concurrent.Future;
 import io.netty.util.concurrent.GenericFutureListener;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.Packet;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientConnection.class)
-public abstract class ClientConnectionMixin
+public abstract class ClientConnectionMixin implements ClientConnectionInterface
 {
     // Add to the packet counter whenever a packet is received.
     @Inject(method = "channelRead0", at = @At("HEAD"))
@@ -28,4 +31,8 @@ public abstract class ClientConnectionMixin
     {
         PacketCounter.totalOut++;
     }
+
+    @Override
+    @Accessor //Compat with adventure-platform-fabric
+    public abstract void setChannel(Channel channel);
 }
