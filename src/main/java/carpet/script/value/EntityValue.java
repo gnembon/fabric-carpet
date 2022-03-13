381010ec42ad02aab69cefb34b1aadf1e90a295f
ch-yx <cuiweixiaoxue63@126.com>
2022-03-14 02:48:13 +0800

    Update EntityValue.java

--- src/main/java/carpet/script/value/EntityValue.java
+++ src/main/java/carpet/script/value/EntityValue.java
@@ -1717,6 +1717,7 @@ public class EntityValue extends Value
                     }
                     item=item.copy();
                     pl.containerMenu.setCarried(item);
+                    pl.containerMenu.broadcastFullState();
                 }
             } catch (CommandSyntaxException e1) {
                 e1.printStackTrace();
