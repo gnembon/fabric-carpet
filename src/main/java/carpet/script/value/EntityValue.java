4a5fe261366196bce22a340c54b5e610719f7e72
ch-yx <cuiweixiaoxue63@126.com>
2022-03-29 00:16:47 +0800

    format

--- src/main/java/carpet/script/value/EntityValue.java
+++ src/main/java/carpet/script/value/EntityValue.java
@@ -1706,9 +1706,6 @@ public class EntityValue extends Value
                     pl.containerMenu.setCarried(item);
                     pl.containerMenu.broadcastFullState();
                 }
-            
-            
-            
         });
         */
         put("skull_dangerous",(e,v)->{
