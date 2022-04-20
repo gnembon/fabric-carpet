e69f170a20aeaddef0c52082673036c2ea3bb8a0
FX - PR0CESS <fx.e.morin@gmail.com>
2022-04-20 11:22:15 -0400

    Fix remaining refactoring issues

--- src/main/java/carpet/mixins/Level_tickMixin.java
+++ src/main/java/carpet/mixins/Level_tickMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.world.level.redstone.NeighborUpdater;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
