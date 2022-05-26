f74120612378feb5f3343d883737b34a1c49ec0f
ch-yx <cuiweixiaoxue63@126.com>
2022-05-26 22:30:54 +0800

    format

--- src/main/java/carpet/mixins/ShulkerBoxBlockEntity_renderFixMixin.java
+++ src/main/java/carpet/mixins/ShulkerBoxBlockEntity_renderFixMixin.java
@@ -1,6 +1,5 @@
 package carpet.mixins;
 
-
 import net.minecraft.client.renderer.blockentity.ShulkerBoxRenderer;
 import net.minecraft.world.level.block.entity.ShulkerBoxBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
@@ -11,20 +10,21 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ShulkerBoxRenderer.class)
-public class ShulkerBoxBlockEntity_renderFixMixin
-{
+public class ShulkerBoxBlockEntity_renderFixMixin {
     private BlockState _be;
+
     @ModifyVariable(method = "render", at = @At("HEAD"), ordinal = 0)
     private ShulkerBoxBlockEntity injected(ShulkerBoxBlockEntity be) {
-        _be=be.getBlockState();
+        _be = be.getBlockState();
         return be;
     }
+
     @Redirect(method = "render", at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/world/level/Level;getBlockState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/block/state/BlockState;"
     ))
-    private net.minecraft.world.level.block.state.BlockState rewrite(net.minecraft.world.level.Level level,net.minecraft.core.BlockPos bp)
-    {
+    private net.minecraft.world.level.block.state.BlockState rewrite(net.minecraft.world.level.Level level,
+            net.minecraft.core.BlockPos bp) {
         return _be;
     }
 }
