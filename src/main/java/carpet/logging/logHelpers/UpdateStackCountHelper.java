98f2a1f34a019e5ab7b8975e23b0a0399850b49b
FX - PR0CESS <fx.e.morin@gmail.com>
2022-06-26 09:42:20 -0400

    Final changes

--- src/main/java/carpet/logging/logHelpers/UpdateStackCountHelper.java
+++ src/main/java/carpet/logging/logHelpers/UpdateStackCountHelper.java
@@ -3,23 +3,17 @@ package carpet.logging.logHelpers;
 import carpet.logging.LoggerRegistry;
 import carpet.logging.TypeLogger;
 import carpet.utils.Messenger;
-import net.minecraft.network.chat.BaseComponent;
+import net.minecraft.network.chat.Component;
 
 public class UpdateStackCountHelper {
 
-
-    @SuppressWarnings("unchecked")
     public static void onStackCount(int count) {
-        if(LoggerRegistry.__updateStackCount) {
-            ((TypeLogger<Integer>) LoggerRegistry.getLogger("updateStackCount")).log(
-                    (TypeLogger.TypeMessageIgnorePlayer<Integer>) (option) -> {
-                if (count >= option) {
-                    return new BaseComponent[]{Messenger.c(
-                            "w Stack finished with: " + count + " updates"
-                    )};
-                }
-                return null;
-            });
-        }
+        if(!LoggerRegistry.__updateStackCount) return;
+        @SuppressWarnings("unchecked")
+        TypeLogger<Integer> stackCountLogger = (TypeLogger<Integer>) LoggerRegistry.getLogger("updateStackCount");
+        stackCountLogger.log((TypeLogger.TypeMessageIgnorePlayer<Integer>) (option) -> {
+            if (count < option) return null;
+            return new Component[]{Messenger.c("w Stack finished with: " + count + " updates")};
+        });
     }
 }
