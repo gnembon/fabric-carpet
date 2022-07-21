86b296b41541309aeb933190a54d71404a010faf
ch-yx <cuiweixiaoxue63@126.com>
2022-07-21 18:57:09 +0800

    add item_display_name

--- src/main/java/carpet/script/api/Auxiliary.java
+++ src/main/java/carpet/script/api/Auxiliary.java
@@ -276,6 +276,10 @@ public class Auxiliary {
             ));
         });
 
+        expression.addContextFunction("item_display_name", 1, (c, t, lv)->{
+            return new FormattedTextValue(EntityValue.getItemStackFromValue(lv.get(0)).getDisplayName());
+        });
+
         expression.addContextFunction("particle_box", -1, (c, t, lv) ->
         {
             CarpetContext cc = (CarpetContext)c;
