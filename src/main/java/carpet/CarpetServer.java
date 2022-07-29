afa1549c540969e3a49bdbcd97e7569d369d135d
altrisi <altrisi.trillosierra@gmail.com>
2022-07-29 21:54:20 +0200

    Complete previous commit

--- src/main/java/carpet/CarpetServer.java
+++ src/main/java/carpet/CarpetServer.java
@@ -88,7 +88,7 @@ public class CarpetServer // static for now - easier to handle all around the co
     {
         CarpetServer.minecraft_server = server;
         // shoudl not be needed - that bit needs refactoring, but not now.
-        SpawnReporter.reset_spawn_stats(server, true);
+        SpawnReporter.resetSpawnStats(server, true);
 
         settingsManager.attachServer(server);
         extensions.forEach(e -> {
