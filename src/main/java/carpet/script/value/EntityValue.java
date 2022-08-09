3522f39f359f7090c5d8c5b5b41c55e164161987
ch-yx <cuiweixiaoxue63@126.com>
2022-08-10 03:34:08 +0800

    better name

--- src/main/java/carpet/script/value/EntityValue.java
+++ src/main/java/carpet/script/value/EntityValue.java
@@ -1702,10 +1702,10 @@ public class EntityValue extends Value
         });
         put("item", (e, v) -> {
                 ItemStack item=getItemStackFromValue(v).copy();
-                if(e instanceof ItemEntity iteme)            
-                    iteme.setItem(item);
-                if(e instanceof ItemFrame iteme)
-                    iteme.setItem(item);  
+                if(e instanceof ItemEntity itementity)            
+                    itementity.setItem(item);
+                if(e instanceof ItemFrame itemframe)
+                    itemframe.setItem(item);  
         });
         // "dimension"      []
         // "count",         []
