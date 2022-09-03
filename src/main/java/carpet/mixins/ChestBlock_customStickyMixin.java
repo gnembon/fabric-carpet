c528695d1ae85de6ebcef4408df63f0dbf07a805
Space Walker <48224626+SpaceWalkerRS@users.noreply.github.com>
2022-09-03 22:49:01 +0200

    Refactor Carpet's sticky blocks code (#1505)
    refactor sticky blocks code

---
+++ src/main/java/carpet/mixins/ChestBlock_customStickyMixin.java
@@ -0,0 +1,45 @@
+package carpet.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import carpet.CarpetSettings;
+import carpet.fakes.BlockBehaviourInterface;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.ChestBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.ChestType;
+
+@Mixin(ChestBlock.class)
+public class ChestBlock_customStickyMixin implements BlockBehaviourInterface {
+
+    @Override
+    public boolean isSticky(BlockState state) {
+        return CarpetSettings.movableBlockEntities;
+    }
+
+    @Override
+    public boolean isStickyToNeighbor(Level level, BlockPos pos, BlockState state, BlockPos neighborPos, BlockState neighborState, Direction dir, Direction moveDir) {
+        if (!neighborState.is((Block)(Object)this)) {
+            return false;
+        }
+
+        ChestType type = state.getValue(ChestBlock.TYPE);
+        ChestType neighborType = neighborState.getValue(ChestBlock.TYPE);
+
+        if (type == ChestType.SINGLE || neighborType == ChestType.SINGLE) {
+            return false;
+        }
+        if (type != neighborType.getOpposite()) {
+            return false;
+        }
+
+        Direction facing = state.getValue(ChestBlock.FACING);
+        Direction neighborFacing = state.getValue(ChestBlock.FACING);
+
+        return facing == neighborFacing;
+    }
+}
