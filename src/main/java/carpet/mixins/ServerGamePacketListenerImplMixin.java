5dd84d0e8575c809009428fa03e9da6f06c46318
Sensei <wellsm488@gmail.com>
2022-10-02 18:03:11 +0100

    Make server net handling run on main thread

--- src/main/java/carpet/mixins/ServerGamePacketListenerImplMixin.java
+++ src/main/java/carpet/mixins/ServerGamePacketListenerImplMixin.java
@@ -3,6 +3,8 @@ package carpet.mixins;
 import carpet.network.CarpetClient;
 import carpet.network.ServerNetworkHandler;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.protocol.PacketUtils;
+import net.minecraft.network.protocol.game.ServerGamePacketListener;
 import net.minecraft.network.protocol.game.ServerboundCustomPayloadPacket;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
@@ -24,6 +26,10 @@ public class ServerGamePacketListenerImplMixin
         ResourceLocation channel = packet.getIdentifier();
         if (CarpetClient.CARPET_CHANNEL.equals(channel))
         {
+            // We should force onto the main thread here
+            // ServerNetworkHandler.handleData can possibly mutate data that isn't
+            // thread safe, and also allows for client commands to be executed
+            PacketUtils.ensureRunningOnSameThread(packet, (ServerGamePacketListener) this, player.getLevel());
             ServerNetworkHandler.handleData(new FriendlyByteBuf(packet.getData().copy()), player);
             ci.cancel();
         }
