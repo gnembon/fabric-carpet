d555260c04ebc844dced55eb54506918cdaf3d12
Ghoulboy <ghoulboy27@gmail.com>
2023-01-20 17:33:26 +0100

    Using enum instead of string

--- src/main/java/carpet/mixins/HugeFungusFeatureMixin.java
+++ src/main/java/carpet/mixins/HugeFungusFeatureMixin.java
@@ -9,14 +9,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
+import static carpet.CarpetSettings.FungusFixMode.*;
+
 @Mixin(HugeFungusFeature.class)
 public class HugeFungusFeatureMixin {
     @ModifyArgs(method = "place", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/feature/HugeFungusFeature;placeStem(Lnet/minecraft/world/level/LevelAccessor;Lnet/minecraft/util/RandomSource;Lnet/minecraft/world/level/levelgen/feature/HugeFungusConfiguration;Lnet/minecraft/core/BlockPos;IZ)V"))
     private void mixin(Args args) {
         boolean natural = !((HugeFungusConfiguration) args.get(2)).planted;
         args.set(5, natural && ((boolean) args.get(5)) ||
-            !natural && (CarpetSettings.thickHugeFungusGrowthFix.equals("all") ||
-            CarpetSettings.thickHugeFungusGrowthFix.equals("vanilla") && ((RandomSource) args.get(1)).nextFloat() < 0.06F)
+            !natural && (CarpetSettings.thickHugeFungusGrowthFix.equals(ALL) ||
+            CarpetSettings.thickHugeFungusGrowthFix.equals(VANILLA) && ((RandomSource) args.get(1)).nextFloat() < 0.06F)
         );
     }
 }
