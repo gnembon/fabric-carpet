42d037db8a0d15747d0f9c4592c80c8ce7d916c1
altrisi <altrisi.trillosierra@gmail.com>
2023-04-22 21:06:24 +0200

    Add missing imports

--- src/main/java/carpet/utils/WoolTool.java
+++ src/main/java/carpet/utils/WoolTool.java
@@ -14,6 +14,8 @@ import net.minecraft.tags.BlockTags;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.MaterialColor;
 import net.minecraft.world.phys.Vec3;
