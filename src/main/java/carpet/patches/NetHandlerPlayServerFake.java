bc498682665e9c66e9028234d9128c4fee76c0ce
silnarm <silnarm@gmail.com>
2023-05-11 18:04:44 +1000

    fake-player-portal-fix: * Re-implement fix for #1190 * Without causing #1712 * Just move the bot on the chunkmap directly.

--- src/main/java/carpet/patches/NetHandlerPlayServerFake.java
+++ src/main/java/carpet/patches/NetHandlerPlayServerFake.java
@@ -1,12 +1,9 @@
 package carpet.patches;
 
-import carpet.mixins.ServerGamePacketListenerImplAccessor;
 import net.minecraft.network.Connection;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.contents.TranslatableContents;
 import net.minecraft.network.protocol.Packet;
-import net.minecraft.network.protocol.game.ServerboundAcceptTeleportationPacket;
-import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.world.entity.RelativeMovement;
@@ -32,24 +29,14 @@ public class NetHandlerPlayServerFake extends ServerGamePacketListenerImpl
             ((EntityPlayerMPFake) player).kill(message);
         }
     }
-    private boolean hasSpawned = false;
 
     @Override
     public void teleport(double d, double e, double f, float g, float h, Set<RelativeMovement> set)
     {
-        boolean isDimensionChange = player.isChangingDimension();
         super.teleport(d, e, f, g, h, set);
-
-        handleAcceptTeleportPacket(
-            new ServerboundAcceptTeleportationPacket( ((ServerGamePacketListenerImplAccessor)this).getAwaitingTeleport() )
-        );
-
-        if (!hasSpawned) {
-            hasSpawned = true;
-        } else if (!isDimensionChange) {
-            handleMovePlayer(
-                new ServerboundMovePlayerPacket.PosRot(player.getX(), player.getY(), player.getZ(), player.getYRot(), player.getXRot(), false)
-            );
+        if (player.serverLevel().getPlayerByUUID(player.getUUID()) != null) {
+            resetPosition();
+            player.serverLevel().getChunkSource().move(player);
         }
     }
 
