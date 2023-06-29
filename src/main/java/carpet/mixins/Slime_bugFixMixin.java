53c7881fa9a159852f830c094438a9cab43521f6
Ghoulboy <ghoulboy27@gmail.com>
2023-06-29 22:45:04 +0200

    Fixing slime and magma cube splitting

---
+++ src/main/java/carpet/mixins/Slime_bugFixMixin.java
@@ -0,0 +1,37 @@
+package carpet.mixins;
+
+import carpet.CarpetSettings;
+import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.animal.Cow;
+import net.minecraft.world.entity.monster.Slime;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(Slime.class)
+public abstract class Slime_bugFixMixin extends Mob {
+    protected Slime_bugFixMixin(EntityType<? extends Mob> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Inject(method = "remove", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;addFreshEntity(Lnet/minecraft/world/entity/Entity;)Z"), locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void mixin(RemovalReason removalReason, CallbackInfo ci, int i, Component component, boolean bl, float f, int j, int k, int l, float g, float h, Slime slime) {
+        if (CarpetSettings.mobConvertKeepNBTTags) {
+            this.getActiveEffects().forEach(slime::addEffect);
+            slime.getAttributes().assignValues(this.getAttributes());
+            slime.setDeltaMovement(this.getDeltaMovement());
+            // slime.setRemainingFireTicks(this.getRemainingFireTicks()); technically a newly spawned entity, not the same one converted
+            slime.setNoGravity(this.isNoGravity());
+            // slime.setPortalCooldown(this.getPortalCooldown()); same here
+            slime.setSilent(this.isSilent());
+            this.getTags().forEach(slime::addTag);
+        }
+    }
+}
