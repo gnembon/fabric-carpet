b0c7fc5610664e0aaad087e4ba3dc72b0627ec40
Ghoulboy <ghoulboy27@gmail.com>
2023-06-29 21:50:50 +0200

    Fixing mooshroom to cow conversion

---
+++ src/main/java/carpet/mixins/MushroomCow_bugFixMixin.java
@@ -0,0 +1,44 @@
+package carpet.mixins;
+
+import carpet.CarpetSettings;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LightningBolt;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.animal.Cow;
+import net.minecraft.world.entity.animal.MushroomCow;
+import net.minecraft.world.entity.monster.Witch;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(MushroomCow.class)
+public abstract class MushroomCow_bugFixMixin extends Mob {
+
+    protected MushroomCow_bugFixMixin(EntityType<? extends Mob> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    @Inject(method = "shear", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/animal/Cow;setInvulnerable(Z)V"), locals = LocalCapture.CAPTURE_FAILSOFT)
+    private void mixin(SoundSource soundSource, CallbackInfo ci, Cow cow) {
+        if (CarpetSettings.mobConvertKeepNBTTags) {
+            this.getActiveEffects().forEach(cow::addEffect);
+            cow.getAttributes().assignValues(this.getAttributes());
+            cow.setDeltaMovement(this.getDeltaMovement());
+            cow.setRemainingFireTicks(this.getRemainingFireTicks());
+            cow.setNoGravity(this.isNoGravity());
+            cow.setPortalCooldown(this.getPortalCooldown());
+            cow.setXRot(this.getXRot());
+            cow.setYRot(this.getYRot());
+            cow.setYBodyRot(this.getVisualRotationYInDegrees());
+            cow.setYHeadRot(this.getYHeadRot());
+            cow.setSilent(this.isSilent());
+            this.getTags().forEach(cow::addTag);
+            cow.setUUID(this.getUUID());
+        }
+    }
+}
