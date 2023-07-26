b4f42f55dbc03e2f47ec3000ef0eeb0382586ec4
altrisi <altrisi.trillosierra@gmail.com>
2023-07-26 14:33:23 +0200

    Extract all logic to specific mixins on modifiedRunLoop
    Still not changed to use vanilla loop and keeping Carpet workarounds

--- src/main/java/carpet/mixins/MinecraftServer_tickspeedMixin.java
+++ src/main/java/carpet/mixins/MinecraftServer_tickspeedMixin.java
@@ -14,17 +14,23 @@ import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.util.thread.ReentrantBlockableEventLoop;
 import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Slice;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.function.BooleanSupplier;
 
+@Debug(export = true)
 @Mixin(value = MinecraftServer.class, priority = Integer.MAX_VALUE - 10)
 public abstract class MinecraftServer_tickspeedMixin extends ReentrantBlockableEventLoop<TickTask> implements MinecraftServerInterface
 {
@@ -92,6 +98,78 @@ public abstract class MinecraftServer_tickspeedMixin extends ReentrantBlockableE
     {
         return false;
     } // target run()
+    
+    @ModifyConstant(method = "runServer", constant = @Constant(longValue = 2000L), allow = 1)
+    private long modifyCantKeepUpLengthCheck(long original) {
+    	return (long)(1000L + 20 * serverTickRateManager.mspt());
+    }
+
+    @ModifyConstant(method = "runServer", constant = @Constant(longValue = 15000L), allow = 1)
+    private long modifyCantKeepUpRecentCheck(long original) {
+    	return (long)(10000L + 100 * serverTickRateManager.mspt());
+    }
+    
+    @ModifyConstant(method = "runServer", constant = @Constant(longValue = 50L),
+    		slice = @Slice(to = @At(value = "NEW", target = "net/minecraft/server/MinecraftServer$TimeProfiler")))
+    private long modifyMsptConstantWithMsptDirectly(long original) {
+    	return (long)serverTickRateManager.mspt();
+    }
+    
+    @ModifyConstant(method = "runServer", constant = @Constant(longValue = 50L),
+    		slice = @Slice(from = @At(value = "NEW", target = "net/minecraft/server/MinecraftServer$TimeProfiler")))
+    private long modifyMsptConstantWithMsThisTick(long original) {
+    	return msThisTick; // TODO figure out why is this and the previous one different
+    }
+    
+    private long msThisTick; // only ever accessed by same thread
+    
+    @Inject(method = "runServer", at = @At(value = "INVOKE", target = "net/minecraft/Util.getMillis()J", ordinal = 1))
+    private void preChecks(CallbackInfo ci) {
+    	//CM deciding on tick speed
+        if (CarpetProfiler.tick_health_requested != 0L)
+        {
+            CarpetProfiler.start_tick_profiling();
+        }
+        //long msThisTick = 0L;
+        long l = 0L;
+        float mspt = serverTickRateManager.mspt();
+        if (serverTickRateManager.isInWarpSpeed() && serverTickRateManager.continueWarp())
+        {
+            // making sure server won't flop after the warp or if the warp is interrupted
+            this.nextTickTime = this.lastOverloadWarning = Util.getMillis();
+            carpetMsptAccum = mspt;
+        }
+        else
+        {
+            if (Math.abs(carpetMsptAccum - mspt) > 1.0f)
+            {
+            	// Tickrate changed. Ensure that we use the correct value.
+            	carpetMsptAccum = mspt;
+            }
+
+            msThisTick = (long)carpetMsptAccum; // regular tick
+            carpetMsptAccum += mspt - msThisTick;
+
+            l = Util.getMillis() - this.nextTickTime;
+        }
+    }
+    
+    @ModifyArg(method = "runServer", at = @At(value = "INVOKE",
+    		target = "net/minecraft/server/MinecraftServer.tickServer(Ljava/util/function/BooleanSupplier;)V"))
+    private BooleanSupplier alwaysRunTasksIfWarping(BooleanSupplier original) {
+    	return serverTickRateManager.isInWarpSpeed() ? () -> true : original;
+    }
+    
+    @Inject(method = "runServer", at = @At(value = "FIELD", target = "net/minecraft/server/MinecraftServer.mayHaveDelayedTasks"))
+    private void runPendingTasksWhenWarping(CallbackInfo ci) {
+    	// clearing all hanging tasks no matter what when warping
+    	if (serverTickRateManager.isInWarpSpeed())
+        {
+            while (this.runEveryTask()) {
+            	Thread.yield(); // TODO check
+            }
+        }
+    }
 
     // Replaced the above cancelled while statement with this one
     // could possibly just inject that mspt selection at the beginning of the loop, but then adding all mspt's to
@@ -109,7 +187,7 @@ public abstract class MinecraftServer_tickspeedMixin extends ReentrantBlockableE
                 CarpetProfiler.start_tick_profiling();
             }
             long msThisTick = 0L;
-            long long_1 = 0L;
+            long l = 0L;
             float mspt = serverTickRateManager.mspt();
             if (serverTickRateManager.isInWarpSpeed() && serverTickRateManager.continueWarp())
             {
@@ -128,22 +206,22 @@ public abstract class MinecraftServer_tickspeedMixin extends ReentrantBlockableE
                 msThisTick = (long)carpetMsptAccum; // regular tick
                 carpetMsptAccum += mspt - msThisTick;
 
-                long_1 = Util.getMillis() - this.nextTickTime;
+                l = Util.getMillis() - this.nextTickTime;
             }
             //end tick deciding
             //smoothed out delay to include mcpt component. With 50L gives defaults.
-            if (long_1 > /*2000L*/1000L+20*mspt && this.nextTickTime - this.lastOverloadWarning >= /*15000L*/10000L+100*mspt)
+            if (l > /*2000L*/1000L + 20*mspt && this.nextTickTime - this.lastOverloadWarning >= /*15000L*/10000L + 100*mspt)
             {
-                long long_2 = (long)(long_1 / mspt);//50L;
-                LOGGER.warn("Can't keep up! Is the server overloaded? Running {}ms or {} ticks behind", long_1, long_2);
-                this.nextTickTime += (long)(long_2 * mspt);//50L;
+                long m = (long)(l / mspt);//50L;
+                LOGGER.warn("Can't keep up! Is the server overloaded? Running {}ms or {} ticks behind", l, m);
+                this.nextTickTime += (long)(m * mspt);//50L;
                 this.lastOverloadWarning = this.nextTickTime;
             }
 
             if (this.debugCommandProfilerDelayStart) {
                 this.debugCommandProfilerDelayStart = false;
                 this.profilerTimings = Pair.of(Util.getNanos(), tickCount);
-                //this.field_33978 = new MinecraftServer.class_6414(Util.getMeasuringTimeNano(), this.ticks);
+                //this.debugCommandProfiler = new MinecraftServer.TimeProfiler(Util.getMeasuringTimeNano(), this.tickCount);
             }
             this.nextTickTime += msThisTick;//50L;
             //TickDurationMonitor tickDurationMonitor = TickDurationMonitor.create("Server");
