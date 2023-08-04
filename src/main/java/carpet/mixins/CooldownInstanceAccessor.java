0f8f791aa1bdde392d664e34988f3c68272d0e7a
Ghoulboy <ghoulboy27@gmail.com>
2023-08-04 15:22:46 +0200

    Remove redundant accessor

--- src/main/java/carpet/mixins/CooldownInstanceAccessor.java
+++ src/main/java/carpet/mixins/CooldownInstanceAccessor.java
@@ -6,9 +6,6 @@ import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(ItemCooldowns.CooldownInstance.class)
 public interface CooldownInstanceAccessor {
-    @Accessor("startTime")
-    int getStartTime();
-
     @Accessor("endTime")
     int getEndTime();
 }
