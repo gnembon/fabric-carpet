e3d11f744b88e5ce1d0db2ec32b2aefd1dcf231b
altrisi <altrisi.trillosierra@gmail.com>
2023-10-13 15:45:26 +0200

    Use the game's constants for NBT types

--- src/main/java/carpet/helpers/InventoryHelper.java
+++ src/main/java/carpet/helpers/InventoryHelper.java
@@ -3,23 +3,10 @@ package carpet.helpers;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
 
+import static net.minecraft.nbt.Tag.*;
+
 public class InventoryHelper
 {
-    // From nbt/NbtElement.java createTag()
-    public static final int TAG_END         = 0;
-    public static final int TAG_BYTE        = 1;
-    public static final int TAG_SHORT       = 2;
-    public static final int TAG_INT         = 3;
-    public static final int TAG_LONG        = 4;
-    public static final int TAG_FLOAT       = 5;
-    public static final int TAG_DOUBLE      = 6;
-    public static final int TAG_BYTEARRAY   = 7;
-    public static final int TAG_STRING      = 8;
-    public static final int TAG_LIST        = 9;
-    public static final int TAG_COMPOUND    = 10;
-    public static final int TAG_INTARRAY    = 11;
-    public static final int TAG_LONGARRAY   = 12;
-
     public static boolean cleanUpShulkerBoxTag(ItemStack stack)
     {
         boolean changed = false;
