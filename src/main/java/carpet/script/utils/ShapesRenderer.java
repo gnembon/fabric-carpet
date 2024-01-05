9acf0920187d62207c5ac4831ffa5b103fc31dcb
ch-yx <cuiweixiaoxue63@126.com>
2024-01-05 18:56:00 +0800

    Update ShapesRenderer.java

--- src/main/java/carpet/script/utils/ShapesRenderer.java
+++ src/main/java/carpet/script/utils/ShapesRenderer.java
@@ -12,6 +12,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.Tesselator;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.blaze3d.vertex.VertexFormat;
+import com.mojang.blaze3d.vertex.VertexSorting;
 import com.mojang.blaze3d.vertex.VertexFormat.Mode;
 import com.mojang.math.Axis;
 import it.unimi.dsi.fastutil.longs.Long2ObjectOpenHashMap;
@@ -20,6 +21,8 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.function.BiFunction;
+
+import org.joml.Matrix4f;
 
 import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
@@ -57,6 +60,7 @@ import net.minecraft.world.phys.Vec3;
 
 public class ShapesRenderer
 {
+    private static final Matrix4f IMATRIX = new Matrix4f();
     private final Map<ResourceKey<Level>, Long2ObjectOpenHashMap<RenderedShape<? extends ShapeDispatcher.ExpiringShape>>> shapes;
     private final Map<ResourceKey<Level>, Long2ObjectOpenHashMap<RenderedShape<? extends ShapeDispatcher.ExpiringShape>>> labels;
     private final Minecraft client;
@@ -176,8 +180,9 @@ public class ShapesRenderer
             matrixStack.popPose();
             //==================
             matrixStack.pushPose();
-            //matrixStack.mulPoseMatrix(matrices.last().pose());
             RenderSystem.applyModelViewMatrix();
+            var ori=RenderSystem.getProjectionMatrix();
+            RenderSystem.setProjectionMatrix(IMATRIX,VertexSorting.ORTHOGRAPHIC_Z);
 
             // lines
             RenderSystem.lineWidth(0.5F);
@@ -196,6 +201,7 @@ public class ShapesRenderer
                 }
             });
             RenderSystem.lineWidth(1.0F);
+            RenderSystem.setProjectionMatrix(ori,VertexSorting.DISTANCE_TO_ORIGIN);
             matrixStack.popPose();
             RenderSystem.applyModelViewMatrix();
 
@@ -206,11 +212,26 @@ public class ShapesRenderer
                     entry -> entry.getValue().isExpired(currentTime)
             );
             labels.get(dimensionType).values().forEach(s -> {
-                if ((!s.shape.debug || entityBoxes) && s.shouldRender(dimensionType))
+                if ((!s.shape.debug || entityBoxes) && s.shouldRender(dimensionType)&& !s.shape.hud)
                 {
                     s.renderLines(matrices, tessellator, bufferBuilder, cameraX, cameraY, cameraZ, partialTick);
                 }
             });
+            //PoseStack matrixStack = RenderSystem.getModelViewStack();
+            matrices.pushPose();
+            matrices.setIdentity();
+            //RenderSystem.applyModelViewMatrix();
+            var ori=RenderSystem.getProjectionMatrix();
+            RenderSystem.setProjectionMatrix(IMATRIX,VertexSorting.ORTHOGRAPHIC_Z);
+            labels.get(dimensionType).values().forEach(s -> {
+                if ((!s.shape.debug || entityBoxes) && s.shouldRender(dimensionType)&& s.shape.hud)
+                {
+                    s.renderLines(matrices, tessellator, bufferBuilder, 0, 0, 0, partialTick);
+                }
+            });
+            matrices.popPose();
+            RenderSystem.applyModelViewMatrix();
+            RenderSystem.setProjectionMatrix(ori,VertexSorting.DISTANCE_TO_ORIGIN);
         }
         RenderSystem.enableCull();
         RenderSystem.depthMask(true);
