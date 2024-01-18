2fabf39f509b6f65086fb8ce07709bb9d6dd0c71
senseiwells <66843746+senseiwells@users.noreply.github.com>
2024-01-18 16:52:11 +0000

    Fix Race Condition Crash Exploit (#1849)

--- src/main/java/carpet/patches/EntityPlayerMPFake.java
+++ src/main/java/carpet/patches/EntityPlayerMPFake.java
@@ -66,7 +66,7 @@ public class EntityPlayerMPFake extends ServerPlayer
             }
         }
         GameProfile finalGP = gameprofile;
-        fetchGameProfile(gameprofile.getName()).thenAccept(p -> {
+        fetchGameProfile(gameprofile.getName()).thenAcceptAsync(p -> {
             GameProfile current = finalGP;
             if (p.isPresent())
             {
@@ -85,7 +85,7 @@ public class EntityPlayerMPFake extends ServerPlayer
             //instance.world.getChunkManager(). updatePosition(instance);
             instance.entityData.set(DATA_PLAYER_MODE_CUSTOMISATION, (byte) 0x7f); // show all model layers (incl. capes)
             instance.getAbilities().flying = flying;
-        });
+        }, server);
         return true;
     }
 
