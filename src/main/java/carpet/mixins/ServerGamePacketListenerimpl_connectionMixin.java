5943a2a24b27ab1320d8641234b49124c4631a61
altrisi <altrisi.trillosierra@gmail.com>
2024-04-28 22:56:15 +0200

    Fix client packets not being received by the server (#1905)

--- src/main/java/carpet/mixins/ServerGamePacketListenerimpl_connectionMixin.java
+++ src/main/java/carpet/mixins/ServerGamePacketListenerimpl_connectionMixin.java
@@ -5,26 +5,29 @@ import carpet.network.ServerNetworkHandler;
 import net.minecraft.network.protocol.PacketUtils;
 import net.minecraft.network.protocol.common.ServerboundCustomPayloadPacket;
 import net.minecraft.network.protocol.game.ServerGamePacketListener;
-import net.minecraft.server.network.ServerCommonPacketListenerImpl;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ServerCommonPacketListenerImpl.class)
-public class ServerCommonPacketListenerimpl_connectionMixin
+@Mixin(ServerGamePacketListenerImpl.class)
+public class ServerGamePacketListenerimpl_connectionMixin
 {
+    @Shadow
+    public ServerPlayer player;
+
     @Inject(method = "handleCustomPayload", at = @At("HEAD"), cancellable = true)
     private void onCustomCarpetPayload(ServerboundCustomPayloadPacket serverboundCustomPayloadPacket, CallbackInfo ci)
     {
-        Object thiss = this;
-        if (thiss instanceof ServerGamePacketListenerImpl impl && serverboundCustomPayloadPacket.payload() instanceof CarpetClient.CarpetPayload cpp) {
+        if (serverboundCustomPayloadPacket.payload() instanceof CarpetClient.CarpetPayload cpp) {
             // We should force onto the main thread here
             // ServerNetworkHandler.handleData can possibly mutate data that isn't
             // thread safe, and also allows for client commands to be executed
-            PacketUtils.ensureRunningOnSameThread(serverboundCustomPayloadPacket, (ServerGamePacketListener) this, impl.player.serverLevel());
-            ServerNetworkHandler.onClientData(impl.player, cpp.data());
+            PacketUtils.ensureRunningOnSameThread(serverboundCustomPayloadPacket, (ServerGamePacketListener) this, player.serverLevel());
+            ServerNetworkHandler.onClientData(player, cpp.data());
             ci.cancel();
         }
     }
