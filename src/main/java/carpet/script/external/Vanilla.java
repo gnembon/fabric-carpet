7a1e152229a2273f90568c9aea76b5d7c20c8cad
gnembon <41132274+gnembon@users.noreply.github.com>
2024-05-16 08:55:50 +0200

    infer release target from minecraft mod container

--- src/main/java/carpet/script/external/Vanilla.java
+++ src/main/java/carpet/script/external/Vanilla.java
@@ -183,8 +183,7 @@ public class Vanilla
         return CarpetSettings.runPermissionLevel;
     }
 
-    @Deprecated
-    public static String MinecraftServer_getReleaseTarget(MinecraftServer server)
+    public static int [] MinecraftServer_getReleaseTarget(MinecraftServer server)
     {
         return CarpetSettings.releaseTarget;
     }
