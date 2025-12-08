be61641c3df742a22f09983a72bdf64ce275ad0a
altrisi <altrisi.trillosierra@gmail.com>
2025-12-08 19:38:54 +0100

    Missed import change to jSpecify

--- src/main/java/carpet/script/CarpetEventServer.java
+++ src/main/java/carpet/script/CarpetEventServer.java
@@ -70,7 +70,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 
-import javax.annotation.Nullable;
+import org.jspecify.annotations.Nullable;
 
 public class CarpetEventServer
 {
