bf55408ff52a29ca0a7c9497c239cc2989bb4381
HerobaneNair <srirambnair@gmail.com>
2026-06-16 08:43:05 -0500

    Fixing isLocalInstanceAuthoritative and getKnownMovement for EntityPlayerMPFakes (#2176)
    Fixes:
    - At least some spear attacks
    - Mace attacks (not verified myself)
    - Fall damage (#2198)
    
    Possibly needs further checking that isLocalInstanceAuthoritative doesn't break other things, but I don't have the time right now.
    
    Co-authored-by: altrisi <altrisi.trillosierra@gmail.com>

---
+++ src/main/java/carpet/mixins/ServerPlayer_fakeLastMovementMixin.java
@@ -0,0 +1,33 @@
+package carpet.mixins;
+
+import org.objectweb.asm.Opcodes;
+import org.spongepowered.asm.mixin.Debug;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.mojang.authlib.GameProfile;
+
+import carpet.patches.EntityPlayerMPFake;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+
+@Mixin(ServerPlayer.class)
+@Debug(export = true)
+public abstract class ServerPlayer_fakeLastMovementMixin extends Player {
+    public ServerPlayer_fakeLastMovementMixin(Level level, GameProfile gameProfile) {
+        super(level, gameProfile);
+        throw new AssertionError();
+    }
+
+    @ModifyExpressionValue(
+        method = {"getKnownMovement", "getKnownSpeed"}, // both because ServerPlayer overrides both to the same "movement" field
+        at = @At(value = "FIELD", target = "lastKnownClientMovement", opcode = Opcodes.GETFIELD),
+        require = 2
+    )
+    private Vec3 bypassClientMovementInfo(Vec3 original) {
+        return ((Player)this) instanceof EntityPlayerMPFake ? super.getKnownMovement() : original;
+    }
+}
