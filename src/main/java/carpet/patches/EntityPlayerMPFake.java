df884ebf876c9d197994a0904d9a0acc207e6bcf
altrisi <altrisi.trillosierra@gmail.com>
2026-06-16 14:51:30 +0200

    Infinite recursion on fall damage calculation due to authoritative

--- src/main/java/carpet/patches/EntityPlayerMPFake.java
+++ src/main/java/carpet/patches/EntityPlayerMPFake.java
@@ -277,11 +277,6 @@ public class EntityPlayerMPFake extends ServerPlayer
     }
 
     @Override
-    protected void checkFallDamage(double y, boolean onGround, BlockState state, BlockPos pos) {
-        doCheckFallDamage(0.0, y, 0.0, onGround);
-    }
-
-    @Override
     public ServerPlayer teleport(TeleportTransition serverLevel)
     {
         super.teleport(serverLevel);
